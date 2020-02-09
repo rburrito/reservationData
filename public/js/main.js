92f5948913be6f7d3ec9dc919f52ccafde36a602
rlaw125 <12490147+rlaw125@users.noreply.github.com>
2020-02-09 15:26:01 -0500

    can access utils data from main.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,10 @@
 // jest won't error out on DOM calls.
 
-//import './utils.js' as utils
-const utils=import('./utils.js')
 // console.log(document.querySelector('p').innerHTML);
 let reservationTimes = document.getElementById('reservation times');
-reservationTimes.innerHTML=JSON.stringify(utils.reservationData);
+
+reservationData = reservations.then((data)=>{
+  console.log(data);
+  reservationTimes.innerHTML=JSON.stringify(data);
+  reduceReservations(data);
+});
--- public/js/utils.js
+++ public/js/utils.js
@@ -5,8 +5,6 @@ function sum (num1, num2) {
 
 
 function createTimes(){
-  // 10 tables
-   // reservation from 1PM to 10PM
   let reservationTimes={};
   for(let i=1; i<10;i++){
     reservationTimes[i.toString()+':00']=10;
@@ -30,28 +28,28 @@ function convertToDoubleDigits(number){
   return number
 }
 
+function getResTime(reservationDate){
+  let resTime=convertToCivTime(reservationDate.getHours())+":"+convertToDoubleDigits(reservationDate.getMinutes());
+  return resTime;
+}
+
 function reduceReservations(data){
   let dates = {};
   for (let i=0; i<data.length; i++){
-  //console.log(data[0].slot);
   let resDate=new Date(data[i].slot);
-  let resTime=convertToCivTime(resDate.getHours())+ ":"+convertToDoubleDigits(resDate.getMinutes());
+  let resTime=getResTime(resDate);
+  let resDay=resDate.toDateString();
+  const day=new Date();
   }
 return dates;
 }
 
-let reservationData;
-
 // API call to get reservation data
+const reservations =
 fetch("http://localhost:3000/reservations")
 .then((response) => response.json())
-.then((data) => {
-  console.log('Success:', data);
-  // call createTimes function to remove timeslots from the ones taken and display them in front-end
 
- reservationData=data;
- reduceReservations(reservationData);
-});
+
 
 //window.onload=function(){
 // export reservationData
