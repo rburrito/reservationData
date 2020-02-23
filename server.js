b22f92ad01b0786db8b6ed8fa01cb5389eb11aae
rlaw125 <12490147+rlaw125@users.noreply.github.com>
2020-02-23 16:30:16 -0500

    added security measures

--- server.js
+++ server.js
@@ -3,11 +3,31 @@ const app = express();
 const port = process.env.PORT || 3000;
 const reservationsRouter = require('./routes/reservations');
 const bodyParser=require('body-parser');
+const helmet=require('helmet');
 
 // serve the homepage from here
 app.use(express.static('public'));
 
-// handle api calls from here
+// only allows our own scripts to be executed
+app.use(helmet.contentSecurityPolicy({
+  directives:{
+    defaultSrc:["'self'"],
+    styleSrc:["'self'"]
+  }
+}));
+
+// blocks reflected XSS
+app.use(helmet.xssFilter());
+
+// sets X-Content-Type options to No Sniff to prevent browser from sniffing MIME for files with misleading extensions and malicious code
+app.use(helmet.noSniff());
+
+// prevents browser from using cached versions of code that may be more vulnerable
+app.use(helmet.noCache());
+
+// prevents clickjacking by preventing frames
+app.use(helmet.frameguard({action:'deny'}));
+
 app.use(bodyParser.urlencoded({ extended:false }));
 
 app.use(bodyParser.json());
